57e13a1e39451339ef28374b071144d529407cb1
hhanny <hannybeeee@gmail.com>
2021-12-19 16:14:15 +0700

    menambahkan folder proyek2

---
+++ proyek2/resources/views/admin/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <h1>Admin Fairy Bouquet</h1>
+    </div>
+</div>
+    
+@endsection
---
+++ proyek2/resources/views/frontend/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.front')
+
+@section('title')
+    Welcome to Fairy Bouquet
+@endsection
+
+@section('content')
+    <h1>Welcome</h1>
+@endsection
